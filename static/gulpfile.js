2f32f9d56a2efd27e748bd1dce6e3d164cfaf60f
AdamWagner <adam@copilotlabs.com>
2014-07-20 15:24:38 -0700

    marketing page updates

---
+++ static/gulpfile.js
@@ -0,0 +1,154 @@
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    order = require('gulp-order'),
+    stylus = require('gulp-stylus'),
+    refresh = require('gulp-livereload'),
+    clean = require('gulp-clean'),
+    lr = require('tiny-lr'),
+    rev = require('gulp-rev'),
+    inject = require('gulp-inject'),
+    path = require('path'),
+    notify = require('gulp-notify'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    server = lr();
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *\
+   Setup inject options
+\* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+function extname(file) {
+  return path.extname(file).slice(1);
+}
+
+injectOptions = {
+    transform: function(filepath, file, index, length){
+        switch(extname(filepath)) {
+          case 'css':
+            return '<link rel="stylesheet" href="{{STATIC_URL}}' + filepath.substring(1) + '">';
+          case 'js':
+            return '<script src="{{STATIC_URL}}' + filepath.substring(1) + '"></script>';
+        }
+    },
+};
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *\
+   Styles
+\* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+gulp.task('inject-css', ['stylus'], function(){
+    gulp.src("./css/*.css", {read: false})
+        .pipe(inject("../templates/base.html",injectOptions))
+        .pipe(gulp.dest('../templates/'))
+        .pipe(refresh(server))
+        // .pipe(notify({message:'CSS build done.'}));
+});
+
+gulp.task('stylus', ['clean-css'], function () {
+    return gulp.src('./stylus/main.styl')
+        .pipe(stylus())
+        .pipe(autoprefixer('last 4 versions'))
+        .pipe(rev())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('clean-css', function() {
+  return gulp.src(['./css/*'], {read: false})
+    .pipe(clean());
+});
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *\
+   Scripts
+\* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+gulp.task('build-js', ['clean-js'], function() {
+    return gulp.src([
+            './js/lib/jquery/dist/jquery.js',
+            './js/lib/angular/angular.js',
+            './js/lib/angular-cookies/angular-cookies.js',
+            './js/lib/lodash/dist/lodash.js',
+            './js/lib/fast-click/fast-click.js',
+
+            './js/src/directives/scrollfix.js',
+
+            './js/src/marketing.js',
+
+            ])
+        .pipe(concat('marketing.js'))
+        .pipe(rev())
+        .pipe(gulp.dest('js/build'))
+});
+
+
+gulp.task('inject-js', ['build-js'], function(){
+    return gulp.src("./js/build/*.js", {read: false})
+        .pipe(inject("../templates/base.html",injectOptions))
+        .pipe(gulp.dest('../templates/'))
+        .pipe(refresh(server))
+        // .pipe(notify({message:'JS build done.'}))
+});
+
+
+gulp.task('clean-js', function() {
+  return gulp.src(['./js/build/*'], {read: false})
+    .pipe(clean());
+});
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *\
+   Compress
+\* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+gulp.task('compress', function(){
+    gulp.src("./js/build/*.js")
+        .pipe(ngmin())
+        .pipe(uglify())
+        .pipe(gulp.dest('js/build'))
+        // .pipe(notify({message:'JS compressed.'}));
+
+    return gulp.src("./css/*.css")
+        .pipe(cssmin())
+        .pipe(gulp.dest('./css/console'))
+        // .pipe(notify({message:'CSS compressed.'}));
+});
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *\
+   Live reload
+\* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+gulp.task('lr-server', function() {
+    server.listen(35729, function(err) {
+        if(err) return console.log(err);
+    });
+});
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  *\
+   Task groups
+\* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+gulp.task('default', function() {
+    gulp.run('lr-server');
+
+    gulp.watch('../**/*.html', function(){
+        gulp.src('../**/*.html')
+            .pipe(refresh(server))
+
+    });
+
+    gulp.watch('stylus/**/*', function(){
+        console.log(' style test');
+        gulp.run('inject-css');
+    });
+
+   gulp.watch('js/src/**/*', function(){
+        gulp.run('inject-js');
+    });
+});
+
+
+
---
+++ static/js/src/marketing.js
@@ -0,0 +1,90 @@
+var app = angular.module('app', ['ngCookies', 'ui.scrollfix']).config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+    }).run(function($http, $cookies){
+        $http.defaults.headers.post['X-CSRFToken'] = $cookies['csrftoken'];
+        $http.defaults.headers.put['X-CSRFToken'] = $cookies['csrftoken'];
+    });
+
+app.controller('Controller', function($scope, $http, $location, $anchorScroll, focus) {
+
+    $scope.showSurvey = true;
+
+    $scope.nopeOptions = [
+        "I'm not really into poetry",
+        "I already use something else",
+        "This doesn't seem useful",
+        "It seems useful, but I wouldn't use it",
+        // "You suck, your children suck, and DraftPress sucks"
+    ];
+
+
+    $scope.selection = [];
+
+    // toggle selection for a given fruit by name
+    $scope.toggleSelection = function toggleSelection(option) {
+        var idx = $scope.selection.indexOf(option);
+
+        // is currently selected
+        if (idx > -1) {
+          $scope.selection.splice(idx, 1);
+        }
+
+        // is newly selected
+        else {
+          $scope.selection.push(option);
+        }
+    }
+
+    $scope.noThanks = _.once(function(){
+        mixpanel.track('Not Interested');
+    });
+
+    $scope.yesPlease = _.once(function(){
+        focus('focusMe');
+        mixpanel.track('Yes Please!');
+    });
+
+    $scope.sendNopeReasons = function(){
+        _.each($scope.selection, function(reason){
+            mixpanel.track('reason: ' + reason);
+        });
+
+        $scope.showSurvey = false;
+        $scope.nopeMode = false;
+        $location.hash('top')
+        $anchorScroll();
+    }
+
+    $scope.user = {name: 'guest', email: 'test@test.com'}
+
+    $scope.signup = function(){
+
+        mixpanel.identify()
+        console.log($scope.email);
+        mixpanel.people.set({$email: $scope.email });
+
+        $scope.showSurvey = false;
+        $scope.yepMode = false;
+        $location.hash('top')
+        $anchorScroll();
+    }
+
+});
+
+app.directive('focusOn', function() {
+   return function(scope, elem, attr) {
+      scope.$on('focusOn', function(e, name) {
+        if(name === attr.focusOn) {
+          elem[0].focus();
+        }
+      });
+   };
+});
+
+app.factory('focus', function ($rootScope, $timeout) {
+  return function(name) {
+    $timeout(function (){
+      $rootScope.$broadcast('focusOn', name);
+    });
+  }
+});
